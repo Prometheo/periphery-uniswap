6c5e0a92bf779cfdfb4163e1280da2b97e594be0
prometheus <olaoyejnr@gmail.com>
2021-07-15 10:10:49 +0100

    change contract owner

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ const WETH = artifacts.require("WETH.sol");
 
 module.exports = async function (deployer, network) {
     let weth;
-    const FACTORY_ADDRESS = '0x228270eCbeA476399ff31A1E2b2c862f16672c43';
+    const FACTORY_ADDRESS = '0xAd7E36dE9a9db9F474Ce9e18D47c45312Ed2580e';
     if(network == 'mainnet') {
         weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
     } else {
